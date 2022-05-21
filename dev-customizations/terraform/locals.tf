02fc5f44c6d7398be514cf8c1082f702c7d61d00
Luigi Di Fraia <luigidifraia@gmail.com>
2022-05-21 21:42:27 +0100

    Replace occurrence of local name that was previously missed

--- dev-customizations/terraform/locals.tf
+++ dev-customizations/terraform/locals.tf
@@ -16,6 +16,6 @@ locals {
   availability_zone_names = slice(sort(data.aws_availability_zones.available.names), 0, var.az_count)
   endpoints               = { for e in var.vpc_endpoints : "com.amazonaws.${var.aws_region}.${e}" => "${e}.${var.aws_region}.amazonaws.com" }
   centralised_endpoints   = { for e in var.centralised_vpc_endpoints : e => "${e}.${var.aws_region}.amazonaws.com" }
-  tgw_map                 = { for e in local.config.tgw_route_tables : "name_${e}" => "${e}" }
+  tgw_map                 = { for e in local.config.tgw_route_tables_propagate : "name_${e}" => "${e}" }
   tgw_route_table         = { for i in sort(keys(local.tgw_map)) : i => data.aws_ec2_transit_gateway_route_table.org_env[i].id }
 }
